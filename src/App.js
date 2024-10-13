3bd2076a489a800b1267e38e1829649e6d4716c9
Matias Rivas <mat500.600@gmail.com>
2024-10-13 13:13:58 -0300

    deploy cambios en rutas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import '../src/css/App.css';
 import '../src/css/home.css';
 import '../src/css/aboutme.css';
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from './components/navbar.jsx';
 import Home from './pages/home';
 import Projects from './pages/projects.jsx';
@@ -15,10 +15,10 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/Portafolio' element={<Home/>}/>
+          <Route path='/' element={<Home/>}/>
           <Route path='/projects' element={<Projects/>}/>
           <Route path='/about' element={<Aboutme/>}/>
-          <Route path='*' element={<h1>Not found!</h1>}/>
+          <Route path='*' element={<Navigate to="/"/>}/>
         </Routes>
       </Router>
 
