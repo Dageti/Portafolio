cde6b3f9ce83e1493b3d03ded2123e5c1f373f44
Matias Rivas <mat500.600@gmail.com>
2024-10-13 13:01:22 -0300

    cambios para deploy

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg  sticky-top">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="#/">My Portfolio</Link>
+        <Link className="navbar-brand" to="/">My Portfolio</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -15,10 +15,10 @@ const Navbar = () => {
           <ul className="navbar-nav ms-auto">
 
             <li className="nav-item">
-              <Link className="nav-link" to="#/projects">Projects</Link>
+              <Link className="nav-link" to="/projects">Projects</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="#/about">About Me</Link>
+              <Link className="nav-link" to="/about">About Me</Link>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="https://www.linkedin.com/in/matias-rivas-gallardo/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
